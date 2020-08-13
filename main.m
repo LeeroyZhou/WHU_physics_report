985b13f01ef3a9e42818e81db637b3d81302adb4
LeeroyZhou <69238692+LeeroyZhou@users.noreply.github.com>
2020-08-13 22:40:45 +0800

    修改了tex和源码

--- main.m
+++ main.m
@@ -1,15 +1,15 @@
 %a=0.5; t1=8; t2=8; t3=7.6;
-% min=1000000;
-% for i=2:1:8 %多起点进行最小值搜索
-%     for j=5:5:60
-%         [i,j]
-%         [x,out]=fminsearch('func1',[i,j,i-1]);
-%         if out<min
-%             real=[x,out];
-%             min=out;
-%         end
-%     end
-% end
+min=1000000;
+for i=2:1:8 %多起点进行最小值搜索
+    for j=5:5:60
+        [i,j]
+        [x,out]=fminsearch('func1',[i,j,i-1]);
+        if out<min
+            real=[x,out];
+            min=out;
+        end
+    end
+end
 
 %[x,out]=fminsearch('func1',[8,5,7]);
 
